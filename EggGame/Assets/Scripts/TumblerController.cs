f83a4465d8899b30cc1c1168a519a13312377b2a
achyutpurohit <an7069@srmist.edu.in>
2023-09-06 09:48:49 +0530

    tumbleDelay

--- EggGame/Assets/Scripts/TumblerController.cs
+++ EggGame/Assets/Scripts/TumblerController.cs
@@ -1,50 +1,89 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TumblerController : MonoBehaviour
 {
+    public float fallSpeed = 1.0f; 
+    public float gravityScale = 1.0f; 
+    public float delayBeforeFalling = 1.0f; 
     private Rigidbody2D rb;
-    private Vector2 initialPosition; 
+    private Vector2 initialPosition;
+    private List<Vector2> fallPath = new List<Vector2>();
     private bool isFalling = false;
     private bool isFallingRight = false;
     private bool isFallingLeft = false;
-    public float fallSpeed = 1.0f;
-    public float destroyAngle = 30.0f;
-    
+    private int replayIndex = 0;
+    private bool isReplayingPath = false;
+    private bool isDelayActive = false; 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        initialPosition = transform.position; 
+        rb.gravityScale = gravityScale; 
+        initialPosition = transform.position;
         FallingRandomDirection();
     }
     void Update()
     {
         if (isFalling)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (!isReplayingPath && Input.GetMouseButtonDown(0))
             {
                 Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 if ((isFallingRight && clickPosition.x > transform.position.x) ||
                     (isFallingLeft && clickPosition.x < transform.position.x))
-                { 
-                    ResetToInitialPosition();
+                {
+                    StartReplayingPath();
                 }
+            }
+            if (isReplayingPath)
+            {
+                if (replayIndex >= 0)
+                {
+                    transform.position = fallPath[replayIndex];
+                    replayIndex--;
+                }
+                else
+                {
+                    isReplayingPath = false; 
+                    StartCoroutine(ResetAfterDelay(delayBeforeFalling)); 
+                }
+            }
+            else if (!isDelayActive) 
+            {
+                
+                fallPath.Add(transform.position);
             }
         }
     }
     void FallingRandomDirection()
     {
-        float randomXForce = Random.Range(-1f, 1f); 
-        rb.AddForce(new Vector2(randomXForce, 0) * fallSpeed, ForceMode2D.Impulse); 
+        float randomXForce = (Random.value < 0.5f) ? -1f : 1f; 
+        rb.velocity = new Vector2(randomXForce, 0) * fallSpeed;
         isFallingRight = randomXForce > 0;
         isFallingLeft = randomXForce < 0;
         isFalling = true;
+    }
+    void StartReplayingPath()
+    {
+        isReplayingPath = true;
+        replayIndex = fallPath.Count - 2; 
+    }
+    System.Collections.IEnumerator ResetAfterDelay(float delay)
+    {
+        isDelayActive = true; 
+        rb.simulated = false; 
+        yield return new WaitForSeconds(delay);
+        rb.simulated = true; 
+        isDelayActive = false; 
+        ResetToInitialPosition();
     }
     void ResetToInitialPosition()
     {
         rb.velocity = Vector2.zero;
         rb.angularVelocity = 0f;
-        transform.position = initialPosition; 
+        transform.position = initialPosition;
+        fallPath.Clear(); 
         FallingRandomDirection();
+        isFalling = true; 
     }
-    
 }
